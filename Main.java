eca841e077043055e31b89b0774259bdbec3dc44
Andrea Munoz <anmunozc@gmail.com>
2026-06-19 23:40:45 -0400

    Clase Envio y Motor Main

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Envio envio1 = new Envio(
+            "ENV001",
+            "Sucursal Norte",
+            15000);
+
+            envio1.mostrarInformacion();
+        }
+}        
