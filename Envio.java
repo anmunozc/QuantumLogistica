0b0e6dc2225c32b77528ed8505dd60f7c11b7749
Andrea Munoz <anmunozc@gmail.com>
2026-06-21 23:48:36 -0400

    feat: encapsulamiento, getter y setter

--- Envio.java
+++ Envio.java
@@ -1,35 +1,43 @@
 public class Envio {
 
-    String idEnvio;
-    String destino;
-    double costoEnvio;
+    //Atributos Privados (encapsulamiento)
+    private String idEnvio;
+    private String destino;
+    private double costoEnvio;
 
+    //Constructor
     public Envio(String idEnvio, String destino, double costoEnvio) {
         this.idEnvio = idEnvio;
         this.destino = destino;
         this.costoEnvio = costoEnvio;
     }
-
+    
+    //Getters pára idEnvio
     public String getIdEnvio() {
         return idEnvio;
     }
 
+    //Setters para idEnvio
     public void setIdEnvio(String idEnvio) {
         this.idEnvio = idEnvio;
     }
 
+    //Getters para destino
     public String getDestino() {
         return destino;
     }
 
+    //Setters para destino
     public void setDestino(String destino) {
         this.destino = destino;
     }
 
+    //Getters para costoEnvio
     public double getCostoEnvio() {
         return costoEnvio;
-    } 
-    
+    }
+
+    //Setters con validacion para costoEnvio
     public void setCostoEnvio(double costoEnvio) {
         if(costoEnvio >= 0) {
             this.costoEnvio = costoEnvio;
@@ -38,6 +46,7 @@ public class Envio {
         }
     }
 
+    // Método para mostrar información del envío
        public void mostrarInformacion() {
         System.out.println("ID Envío: " + idEnvio);
         System.out.println("Destino: " + destino);
--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Creación del objeto
         Envio envio1 = new Envio(
                 "ENV001",
                 "Sucursal Norte",
                 15000);
 
+        // Prueba de validación
         envio1.setCostoEnvio(-5000);
 
+        // Mostrar información del envío
         envio1.mostrarInformacion();
     }
 }
-
